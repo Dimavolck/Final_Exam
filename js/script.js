4da9bec351119138a278dbeb8731edaa999fa857
Dimavolck <71402257+Dimavolck@users.noreply.github.com>
2020-12-09 17:41:27 -0800

    update
    tried my best

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("label").style.visibility = "hidden";
+document.getElementById("effect").style.visibility = "hidden";
+document.getElementById("butt").style.visibility = "hidden";
+
+function add_image(){
+    for (i = 1; i <= 5; i++){
+        
+    }
+}
